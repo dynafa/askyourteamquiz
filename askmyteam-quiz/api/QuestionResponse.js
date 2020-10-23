e53c1ff7b71c2b924d27fd085ff547cafd9a91ad
sjdolley <garfunkalman2@gmail.com>
2020-10-24 10:12:27 +1300

    params updated

--- askmyteam-quiz/api/QuestionResponse.js
+++ askmyteam-quiz/api/QuestionResponse.js
@@ -18,12 +18,12 @@ module.exports.create = (event, context, callback) => {
   }
 
   // need to redo this as an update question api call, this will not work as we don't wish to create a new user
-  createResponse(userInfo(userID, name, organisationName, password))
+  createResponse(userInfo(question_ID, name, given_answer, quiz_ID))
   .then(res => {
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        message: `Sucessfully submitted candidate with user name ${name}`,
+        message: `Sucessfully submitted answer ${given_answer} for ${name} on question number ${question_ID}`,
         userId: res.id
       })
     });
@@ -39,21 +39,76 @@ module.exports.create = (event, context, callback) => {
   });
 };
 
-const createUser = user => {
-  console.log('Submitting User');
-  const userInfo = {
-    TableName: process.env.USERTABLE,
-    Item: user,
+const createResponse = response => {
+  console.log('Submitting Response');
+  const responseInfo = {
+    TableName: process.env.DYNAMODB_RESPONSE,
+    Item: response,
   };
   return dynamoDb.create(userInfo).promise()
-    .then(res=>user);
+    .then(res=>response);
 }
 
-const userInfo = (name, organisationName, password) => {
+const responseInfo = (question_ID, name, quiz_ID, given_answer) => {
   return {
-    userID: uuid.v1(),
     name: name,
-    organisationName: organisationName,
-    password: password,
+    question_ID: question_ID,
+    quiz_ID: quiz_ID,
+    given_answer,
   };
+};
+
+// get all responses by quiz_ID
+module.exports.list = (event, context, callback) => {
+  var params ={
+    TableName: process.env.DYNAMODB_RESPONSE_TABLE,
+    Key: {
+        quiz_ID: event.pathParameters.quiz_ID,
+    },
+    ProjectionExpression: "Question_ID, Quiz_ID, Given_answer, Question_correct_answer, Question_choice_type, Answer_correct"
+  };
+
+  console.log("Scanning User Table");
+  const onScan = (err, data) => {
+    if (err) {
+      console.log('Scan failed on data loading. Error JSON', JSON.stringify(err, null, 2));
+      callback(err);
+    } else {
+      console.log("Successful scan");
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result.Items),
+      });
+    }
+  };
+  dynamoDb.scan(params, onScan);
+};
+
+//delete a response by question_ID
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.USERTABLE,
+    Key: {
+      question_ID: event.pathParameters.question_ID,
+    },
+  };
+
+dynamoDb.delete(params, (error) => {
+  //handle errors
+  if (error) {
+    console.error(error);
+    callback(null, {
+      statusCode: error.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t remove the question.',
+    });
+    return;
+}
+// the response
+const response = {
+  statusCode: 200,
+  body: JSON.stringify({}),
+};
+callback(null, response);
+});
 };
